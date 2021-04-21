5f6b82c6a9ad4a65c341cb4d4ea2b4d38125231d
Alan Claughan <git@claughan.com>
2021-04-21 06:56:25 +0200

    changed the co-ordinates to show True

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def main():
     endpoint = "https://api.sunrise-sunset.org/json"
     params = \
         {
-            "lat": -26.0452268,
-            "lng": 28.0196064,
+            "lat": 40.7579787,
+            "lng": -73.9877366,
             "formatted": 0
         }
 
@@ -29,23 +29,22 @@ def main():
 
     now = datetime.now().hour
     sunrise, sunset = get_hour('sunrise', data), get_hour('sunset', data)
-    dark = is_dark(now, sunrise,sunset)
+    dark = is_dark(now, sunrise, sunset)
 
+    print(
+        f"    now: {now}\nsunrise: {sunrise}\n sunset: {sunset}\n   dark: {dark}")
 
-
-    print(f"    now: {now}\nsunrise: {sunrise}\n sunset: {sunset}\n   dark: {dark}")
 
 def get_hour(key, data):
     # extracts the hour form the datetime received and adds 2 to adjust the timezone
     return int(data['results'][key].split('T')[1].split(':')[0]) + 2
+
 
 def is_dark(hour, sunrise, sunset):
     if hour < sunrise or hour > sunset:
         return True
     else:
         return False
-
-
 
 
 if __name__ == '__main__':
